9f05d7df8f427b990f968a34646dfa9910d8fe76
Alex Cowling <alexcowling3@gmail.com>
2019-06-26 19:56:26 -0700

    Alex added updates to the profile account page

--- buddy/server.py
+++ buddy/server.py
@@ -9,6 +9,7 @@ bcrypt = Bcrypt(app)
 app.secret_key = "keep it secret"
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
+
 @app.route('/')
 def root():
     return render_template('index.html')
@@ -31,7 +32,6 @@ def signup():
     results = mysql.query_db(query, data)
     print(results)
 
-
     # form validations
     if len(results) > 0:
         is_valid = False
@@ -49,6 +49,10 @@ def signup():
         is_valid = False
         flash("Please provide your preferred pronoun.", 'pronoun')
 
+    if len(request.form['about']) < 1:
+        is_valid = False
+        flash("Please share a short bio about yourself!", 'about')
+
     if len(request.form['password']) < 8:
         is_valid = False
         flash("Your password must be at least 8 characters.", 'password')
@@ -56,7 +60,7 @@ def signup():
     if not request.form['password'] == request.form['password_confirmation']:
         is_valid = False
         flash("Passwords don't match.", 'password')
-    
+
     if len(request.form['telephone']) < 10:
         is_valid = False
         flash("10-digit phone number required.", 'telephone')
@@ -67,11 +71,12 @@ def signup():
         pw_hash = bcrypt.generate_password_hash(request.form['password'])
         print(pw_hash)
         mysql = connectToMySQL('buddy')
-        query = "INSERT INTO users (first_name, last_name, pronoun, email, telephone, password, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, %(pronoun)s, %(email)s, %(telephone)s, %(password)s, NOW(), NOW());"
+        query = "INSERT INTO users (first_name, last_name, pronoun, about, email, telephone, password, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, %(pronoun)s, %(about)s, %(email)s, %(telephone)s, %(password)s, NOW(), NOW());"
         data = {
             'first_name': request.form['first_name'],
             'last_name': request.form['last_name'],
             'pronoun': request.form['pronoun'],
+            'about': request.form['about'],
             'email': request.form['email'],
             'telephone': request.form['telephone'],
             'password': pw_hash
@@ -105,17 +110,35 @@ def member():
         return redirect('/')
 
 
-
 # route to render page to create new account
 @app.route('/signup')
 def signin_process():
     return render_template('signup.html')
 
 
-
 @app.route('/welcome')
 def buddy_Welcome():
-    return render_template('userprofile.html')
+
+    userid = session['userid']
+    mysql = connectToMySQL('buddy')
+    query = "SELECT first_name FROM users WHERE id=" +str(userid)
+    first_name = mysql.query_db(query)
+
+    mysql = connectToMySQL('buddy')
+    query = "SELECT last_name FROM users WHERE id=" +str(userid)
+    last_name = mysql.query_db(query)
+
+    mysql = connectToMySQL('buddy')
+    query = "SELECT about FROM users WHERE id=" +str(userid)
+    about = mysql.query_db(query)
+
+    return render_template('userprofile.html', first_name=first_name, last_name=last_name, about=about)
+
+
+@app.route('/request')
+def accepted():
+    return render_template('accepted.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
